ac6e6a3bd7d174fef1a29ac9a5124032bd464f59
gladysbenitez <gladys.benitez@galvanize.com>
2024-05-06 15:14:35 -0500

    mobile main fix

--- src/MainContent.js
+++ src/MainContent.js
@@ -7,24 +7,21 @@ const MainContent = () => {
   const [muted, setMuted] = useState(true);  // Controls the audio muting
 
   return (
-    <div style={{ position: 'relative', width: '100%', height: '100vh', overflow: 'hidden' }}>
-      <video autoPlay playsinline disablePictureInPicture loop muted style={{ position: 'absolute', width: '100%', height: '100%', top: '0', left: '0', objectFit: 'cover' }}>
-        <source src="https://www.sugokuii-events.com/worldofsugo-img/seawave.mp4" type="video/mp4" />
-        Your browser does not support the video tag.
-      </video>
-      <div style={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', zIndex: '10', color: 'white', textAlign: 'center' }}>
-        <h1>Coming Soon...</h1> 
-        <h1>- Capri, Italy -</h1>
-        <h1>2025</h1>
-        <audio autoPlay loop muted={!muted}>
-          <source src="https://www.sugokuii-events.com/media/abbronzatissima-sugoworld.mp3" type="audio/mpeg" />
-        </audio>
-
-        {/* <button onClick={() => setMuted(!muted)} style={{ padding: '10px 20px', fontSize: '16px', cursor: 'pointer' }}>
-          {muted ? 'Unmute' : 'Mute'}
-        </button> */}
-      </div>
-
+    <div className='main-content'>
+        <div className='backgroundvideo'>
+            <video autoPlay playsinline disablePictureInPicture loop muted style={{ position: 'absolute', minWidth: '100%', minHeight: '100%', top: '0', left: '0', objectFit: 'cover', zIndex:-1 }}>
+                <source src="https://www.sugokuii-events.com/worldofsugo-img/seawave.mp4" type="video/mp4" />
+                Your browser does not support the video tag.
+            </video>
+        <div className="center-container">
+            <h1>Coming Soon...</h1> 
+            <h1>- Capri, Italy -</h1>
+            <h1>2025</h1>
+            <audio autoPlay loop muted={!muted}>
+            <source src="https://www.sugokuii-events.com/media/abbronzatissima-sugoworld.mp3" type="audio/mpeg" />
+            </audio>
+        </div>
+        </div>
     </div>
   );
 };
