1b1e4b16ad37137f3661c21ff7db08a9d7ccac19
gladysbenitez <gladys.benitez@galvanize.com>
2024-08-14 17:36:09 -0500

    home page video on smaller screen

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,25 +1,25 @@
 import React, { useState } from 'react';
 import './HomePage.css';
 
-
 const HomePage = () => {
-    // eslint-disable-next-line no-unused-vars
-  const [muted, setMuted] = useState(true);  // Controls the audio muting
+  // eslint-disable-next-line no-unused-vars
+  const [muted, setMuted] = useState(true); // Controls the audio muting
 
   return (
     <div className='main-content'>
-        <video className="home-background-media" autoPlay loop muted playsInline>
-          <source src="https://www.sugokuii-events.com/worldofsugo-img/seawave.mp4" type="video/mp4" />
-        </video>
-        <img className='home-background-media' src='/images/Screenshot 2024-05-06 at 4.58.11 PM.png' alt="Background"></img>
-        <div className="center-container">
-            <h1 className='header-text'>Coming Soon...</h1> 
-            <h1 className='header-text'>- Capri, Italy -</h1>
-            <h1 className='header-text'>2025</h1>
-            <audio autoPlay loop muted={!muted}>
-            <source src="https://www.sugokuii-events.com/media/abbronzatissima-sugoworld.mp3" type="audio/mpeg" />
-            </audio>
-        </div>
+      <video className="home-background-media" autoPlay loop muted playsInline>
+        <source src="https://www.sugokuii-events.com/worldofsugo-img/seawave.mp4" type="video/mp4" />
+      </video>
+      {/* The img tag can be used as a fallback, but if the video should always play, you can remove it */}
+      {/* <img className='home-background-media' src='/images/Screenshot 2024-05-06 at 4.58.11 PM.png' alt="Background" /> */}
+      <div className="center-container">
+        <h1 className='header-text'>Coming Soon...</h1> 
+        <h1 className='header-text'> Capri, Italy </h1>
+        <h1 className='header-text'>2025</h1>
+        <audio autoPlay loop muted={!muted}>
+          <source src="https://www.sugokuii-events.com/media/abbronzatissima-sugoworld.mp3" type="audio/mpeg" />
+        </audio>
+      </div>
     </div>
   );
 };
